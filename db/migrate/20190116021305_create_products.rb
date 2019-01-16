a41608ba8e854d70d182fea579534ebbecf9b0f1
Manjuka Weerasinghe <manjukaweerasinghe@Manjukas-MacBook-Pro.local>
2019-01-15 21:44:04 -0500

    Move old project

---
+++ db/migrate/20190116021305_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.integer :quantity
+      t.decimal :price, precision: 12, scale: 3
+      t.boolean :active
+
+      t.timestamps
+    end
+  end
+end
